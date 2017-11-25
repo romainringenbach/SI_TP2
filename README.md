5f4762d305a5ab64e6cd58aaddba5fa590aec665
fonspa <fonspa@gmail.com>
2017-11-25 18:59:07 +0100

    Corrected camera perspective, canvas size. Worked on shader menu. Added key event listener for menu.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SI_TP2
+
+## Controls
+
+1. Shaders Menu
+
+   Pour faire apparaître et disparaître le menu des shaders, appuyez sur la touche _suppr_ du pavé numérique.
--- atomicGL2Context.js
+++ atomicGL2Context.js
@@ -10,57 +10,57 @@
 class atomicGL2Context {
 	// constructor
 	//------------------------
-	constructor(){
+	constructor() {
 		// attributes
 		// -------------------------------------------------
 		// GL context
-		this.gl ;
+		this.gl;
 		// GL context size
-		this.viewportWidth ;
-		this.viewportHeight ;
+		this.viewportWidth;
+		this.viewportHeight;
 
 		// lights
 		// ambient
-		this.ambientLightColor = [0.01,0.01,0.1];
+		this.ambientLightColor = [0.01, 0.01, 0.1];
 		// omniDirLight
-		this.omniLightColor = [] ;
-		this.omniLightLocation = [] ;
+		this.omniLightColor = [];
+		this.omniLightLocation = [];
 		this.omniLightNumber = 0;
-	 
+
 		// GLtexture
-		this.GLtexture = [] ;
-	
+		this.GLtexture = [];
+
 		// -------------------------------------------------
 		// scene assets
 		// -------------------------------------------------
 		// shaders
 		this.shaderPrograms = [];
 		// textures
-		this.textures = [] ;
+		this.textures = [];
 		// shapes
-		this.shapes = [] ;
+		this.shapes = [];
 		// scene graph
-		this.scenegraph = null ;
+		this.scenegraph = null;
 	}
-	
+
 	// methods
 	// --------------------------------------------------
 	// initGL(canvas)
 	//---------------------------
 	// inputs: 	canvas: html canvas
 	// 			backgroundColor: [float, float, float]
-	initGL(canvas,backgroundColor) {
+	initGL(canvas, backgroundColor) {
 		// debug
 		//console.log("atomicGLContext::initGL");
 		// recover canvas openGL
-        try {
-            this.gl = canvas.getContext("webgl");
-            this.viewportWidth = canvas.width;
-            this.viewportHeight = canvas.height;
-        } catch (e) {}
-        if (!this.gl) { // error in the initialisation of GL context
-            alert("atomicGLContext::Could not initialise WebGL");
-        }
+		try {
+			this.gl = canvas.getContext("webgl");
+			this.viewportWidth = canvas.width;
+			this.viewportHeight = canvas.height;
+		} catch (e) { }
+		if (!this.gl) { // error in the initialisation of GL context
+			alert("atomicGLContext::Could not initialise WebGL");
+		}
 		else { // GL context initialised -> first init (background color, DEPTH_TEST)
 			this.gl.clearColor(backgroundColor[0], backgroundColor[1], backgroundColor[2], 1.0);
 			this.gl.enable(this.gl.DEPTH_TEST);
@@ -75,56 +75,59 @@ class atomicGL2Context {
 		this.GLtexture.push(this.gl.TEXTURE5);
 		this.GLtexture.push(this.gl.TEXTURE6);
 		this.GLtexture.push(this.gl.TEXTURE7);
-    }
+	}
 	// initDraw()
 	//---------------------------
 	initDraw() {
 		// debug
 		//console.log("atomicGLContext::initDraw");
-		this.gl.viewport(0, 0, this.viewportWidth, this.viewportHeight);
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+		resizeCanvasToDisplaySize(this.gl.canvas, window.devicePixelRatio);
+		// this.gl.viewport(0, 0, this.viewportWidth, this.viewportHeight);
+		this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+
+		this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 	}
-	
+
 	// pushLight(lightPos,lightColor)
 	// ---------------------------
 	// inputs: 	lightPos : float3 - light position
 	// 			lightColor: float3 - light color
-	pushLight(lightPos,lightColor){
+	pushLight(lightPos, lightColor) {
 		// debug
 		//console.log("atomicGLContext::pushLight");		
 		// increase Light number
 		this.omniLightNumber = this.omniLightNumber + 1;
 		// set data
-		this.omniLightLocation.push(lightPos[0]) ;
-		this.omniLightLocation.push(lightPos[1]) ;
-		this.omniLightLocation.push(lightPos[2]) ;
-		this.omniLightColor.push(lightColor[0]) ;
-		this.omniLightColor.push(lightColor[1]) ;
-		this.omniLightColor.push(lightColor[2]) ;
-		}
-	
+		this.omniLightLocation.push(lightPos[0]);
+		this.omniLightLocation.push(lightPos[1]);
+		this.omniLightLocation.push(lightPos[2]);
+		this.omniLightColor.push(lightColor[0]);
+		this.omniLightColor.push(lightColor[1]);
+		this.omniLightColor.push(lightColor[2]);
+	}
+
 	// pushProgram(prog)
 	// ---------------------------
 	// inputs: prog - atomicGLShader
-	pushProgram(prog){ 
+	pushProgram(prog) {
 		// debug
 		//console.log("atomicGLContext::pushProgram");
-		this.shaderPrograms.push(prog); 
-		var id =  this.shaderPrograms.length -1
+		this.shaderPrograms.push(prog);
+		var id = this.shaderPrograms.length - 1
 		// debug
 		//console.log("-- atomicGLContext::pushProgram("+prog.name+")-> index:"+id);
-		return  id ;
+		return id;
 	}
-	
+
 	// indexOfTexture
 	// ---------------------------------------
 	// input: 	id - string: id name of texture
 	// output:	int - index of texture in this.textures
-	indexOfTexture(id){
-		var res = -1 ;
-		for (var i=0; i<this.textures.length;i++){
-			var idTex = this.textures[i].id ;
-			if (id==idTex){res = i;break}
+	indexOfTexture(id) {
+		var res = -1;
+		for (var i = 0; i < this.textures.length; i++) {
+			var idTex = this.textures[i].id;
+			if (id == idTex) { res = i; break }
 		}
 		return res;
 	}
@@ -133,24 +136,24 @@ class atomicGL2Context {
 	// ---------------------------------------
 	// input: 	id - string: id name of shader
 	// output:	int - index of shader in this.shaders
-	indexOfShader(id){
-		var res = -1 ;
-		for (var i=0; i<this.shaderPrograms.length;i++){
-			var shadername = this.shaderPrograms[i].name ;
-			if (id==shadername){res = i;break}
+	indexOfShader(id) {
+		var res = -1;
+		for (var i = 0; i < this.shaderPrograms.length; i++) {
+			var shadername = this.shaderPrograms[i].name;
+			if (id == shadername) { res = i; break }
 		}
 		return res;
 	}
-	
+
 	// indexOfShape
 	// ---------------------------------------
 	// input: 	id - string: id name of shape
 	// output:	int - index of shape in this.shapes
-	indexOfShape(id){
-		var res = -1 ;
-		for (var i=0; i<this.shapes.length;i++){
-			var shapename = this.shapes[i].name ;
-			if (id==shapename){res = i;break}
+	indexOfShape(id) {
+		var res = -1;
+		for (var i = 0; i < this.shapes.length; i++) {
+			var shapename = this.shapes[i].name;
+			if (id == shapename) { res = i; break }
 		}
 		return res;
 	}
