766c8deaaf88d6c379599f1882bf1693d3119827
fonspa <fonspa@gmail.com>
2017-11-26 14:49:14 +0100

    Begin handling webgl context loss. Added webgl-debug lib. Tidying code.

--- atomicGL2Shader.2.js
+++ atomicGL2Shader.2.js
@@ -11,20 +11,20 @@
 // atomicGL2ShaderLoaderScriptInLine extends atomicGL2ShaderLoader
 // atomicGL2ShaderLoaderScriptXML extends atomicGL2ShaderLoader
 //----------------------------------------------------------------------------------------
-class atomicGL2ShaderLoader{
-	constructor(){
+class atomicGL2ShaderLoader {
+	constructor() {
 		this.vertexShaderSRC = "";
-		this.fragmentShaderSRC = "" ;
+		this.fragmentShaderSRC = "";
 	}
-	getVertex(){return this.vertexShaderSRC;}
-	getFragment(){return this.fragmentShaderSRC;}
+	getVertex() { return this.vertexShaderSRC; }
+	getFragment() { return this.fragmentShaderSRC; }
 }
 
 class atomicGL2ShaderLoaderScriptInLine extends atomicGL2ShaderLoader {
-	constructor(vertexShaderID,fragmentShaderID){
+	constructor(vertexShaderID, fragmentShaderID) {
 		super();
-		this.vertexShaderSRC = this.getShaderSRC(vertexShaderID) ;
-		this.fragmentShaderSRC = this.getShaderSRC(fragmentShaderID) ;
+		this.vertexShaderSRC = this.getShaderSRC(vertexShaderID);
+		this.fragmentShaderSRC = this.getShaderSRC(fragmentShaderID);
 	}
 	// getShaderSRC
 	// -------------------------
@@ -32,17 +32,17 @@ class atomicGL2ShaderLoaderScriptInLine extends atomicGL2ShaderLoader {
 	getShaderSRC(id) {
 		var shader;
 		var str = "";
-        // shader source
+		// shader source
 		var shaderScript = document.getElementById(id);
-        if (!shaderScript) {
-			alert("Could not find shader source:"+id);
+		if (!shaderScript) {
+			alert("Could not find shader source:" + id);
 			return null;
 		}
 		else {
 			var str = "";
 			var k = shaderScript.firstChild;
 			while (k) {
-				if (k.nodeType == 3) {str += k.textContent;}
+				if (k.nodeType == 3) { str += k.textContent; }
 				k = k.nextSibling;
 			}
 		}
@@ -54,31 +54,31 @@ class atomicGL2ShaderLoaderScriptInLine extends atomicGL2ShaderLoader {
 }
 
 class atomicGL2ShaderLoaderScriptXML extends atomicGL2ShaderLoader {
-		constructor(xmlfile){
+	constructor(xmlfile) {
 		super();
-		this.vertexShaderSRC = this.getShaderSRC(xmlfile,"vertex") ;
-		this.fragmentShaderSRC = this.getShaderSRC(xmlfile,"fragment") ;
+		this.vertexShaderSRC = this.getShaderSRC(xmlfile, "vertex");
+		this.fragmentShaderSRC = this.getShaderSRC(xmlfile, "fragment");
 	}
 	// getShaderSRC
 	// -------------------------
 	// get shader source
-	getShaderSRC(xmlfile,type) {
+	getShaderSRC(xmlfile, type) {
 		// loadXMLfile
-		var xmlhttp=new XMLHttpRequest();
-		xmlhttp.open("GET",xmlfile,false);
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", xmlfile, false); //synchronous
 		xmlhttp.send();
-		var xmlDoc=xmlhttp.responseXML;
-		var str = "" ;
+		var xmlDoc = xmlhttp.responseXML;
+		var str = "";
 
-		switch (type){
-			case "vertex" :
+		switch (type) {
+			case "vertex":
 				var xvertex = xmlDoc.getElementsByTagName("VERTEX");
-				str = xvertex[0].childNodes[0].data ;
-			break ;
-			case "fragment" :
+				str = xvertex[0].childNodes[0].data;
+				break;
+			case "fragment":
 				var xfragment = xmlDoc.getElementsByTagName("FRAGMENT");
-				str = xfragment[0].childNodes[0].data ;
-			break ;
+				str = xfragment[0].childNodes[0].data;
+				break;
 		}
 		// debug
 		// ----------------------
@@ -87,11 +87,11 @@ class atomicGL2ShaderLoaderScriptXML extends atomicGL2ShaderLoader {
 	}
 }
 
-class atomicGL2Shader{
-	constructor(nname){this.name=nname;}
+class atomicGL2Shader {
+	constructor(nname) { this.name = nname; }
 }
 
-class  atomicGL2MatShader extends atomicGL2Shader{
+class atomicGL2MatShader extends atomicGL2Shader {
 	// constructor
 	//------------------------
 	// inputs:	nname: 		shader name - string
@@ -105,37 +105,37 @@ class  atomicGL2MatShader extends atomicGL2Shader{
 	//									uPointLightPosition0|1|2 required per light in the shader
 	//									uPointLightColor0|1|2 required per light in the shader
 
-	constructor(nname, agl,shaderloader,nnbTex,nnbLights){
+	constructor(nname, agl, shaderloader, nnbTex, nnbLights) {
 		// attributes
 		// -------------------------------------------------
 		super(nname);
 		// nbTex
-		this.nbTex = nnbTex ;
+		this.nbTex = nnbTex;
 		// nbLights
-		this.nbLight = nnbLights ;
+		this.nbLight = nnbLights;
 		// program shader
-		this.program ;
+		this.program;
 
 		// attributes
 		// --------------------------
-			this.vertexPositionAttribute ;
-			this.vertexNormalAttribute ;
-			this.vertexColorAttribute ;
-			this.texCoordAttribute ;
+		this.vertexPositionAttribute;
+		this.vertexNormalAttribute;
+		this.vertexColorAttribute;
+		this.texCoordAttribute;
 		// uniforms
 		// --------------------------
-			// uniform Matrices
-			this.pMatrixUniform ;
-			this.mvMatrixUniform ;
-			this.nMatrixUniform ;
-			// light
-			this.ambientColorUniform ;
-			this.pointLightLocationUniform = [] ;
-			this.pointLightColorUniform = [] ;
-			// texture -sampler
-			this.samplerUniform = [] ;
+		// uniform Matrices
+		this.pMatrixUniform;
+		this.mvMatrixUniform;
+		this.nMatrixUniform;
+		// light
+		this.ambientColorUniform;
+		this.pointLightLocationUniform = [];
+		this.pointLightColorUniform = [];
+		// texture -sampler
+		this.samplerUniform = [];
 
-	this.build(agl,shaderloader);
+		this.build(agl, shaderloader);
 	}
 
 	// methods
@@ -145,48 +145,51 @@ class  atomicGL2MatShader extends atomicGL2Shader{
 	// inputs:	agl: GL context
 	// 			vstr: vertex shader  - string
 	// 			fstr: fragment shader - string
-    createProgram(agl,vstr,fstr) {
+	createProgram(agl, vstr, fstr) {
 		// debug
 		//console.log("atomicGLShader::createProgram ("+fragmentShaderID+","+vertexShaderID+")");
 		// creation des shaders
 		// vertex
-        var vertexShader = agl.gl.createShader(agl.gl.VERTEX_SHADER);
+		var vertexShader = agl.gl.createShader(agl.gl.VERTEX_SHADER);
 		// set source
-        agl.gl.shaderSource(vertexShader, vstr);
+		agl.gl.shaderSource(vertexShader, vstr);
 		// shader compilation
-        agl.gl.compileShader(vertexShader);
+		agl.gl.compileShader(vertexShader);
 		// debug
-		console.log("atomicGLShader2::createProgram -> compile result: "+agl.gl.getShaderParameter(vertexShader, agl.gl.COMPILE_STATUS));
-		// check erreur de compilation
-        if (!agl.gl.getShaderParameter(vertexShader, agl.gl.COMPILE_STATUS)) {
-            alert(agl.gl.getShaderInfoLog(vertexShader));
-            return null;
-        }
+		console.log("atomicGLShader2::createProgram -> compile result: " + agl.gl.getShaderParameter(vertexShader, agl.gl.COMPILE_STATUS));
+		// check erreur de compilation sans perte de contexte
+		if (!agl.gl.getShaderParameter(vertexShader, agl.gl.COMPILE_STATUS)
+	&& !agl.gl.isContextLost()) {
+			alert("Error compiling shader:\n" + agl.gl.getShaderInfoLog(vertexShader));
+			return null;
+		}
 		// fragment
 		var fragmentShader = agl.gl.createShader(agl.gl.FRAGMENT_SHADER);
 		// set source
-        agl.gl.shaderSource(fragmentShader, fstr);
+		agl.gl.shaderSource(fragmentShader, fstr);
 		// shader compilation
-        agl.gl.compileShader(fragmentShader);
+		agl.gl.compileShader(fragmentShader);
 		// debug
-		console.log("atomicGLShader2::createProgram -> compile result: "+agl.gl.getShaderParameter(fragmentShader, agl.gl.COMPILE_STATUS));
-		// check erreur de compilation
-        if (!agl.gl.getShaderParameter(fragmentShader, agl.gl.COMPILE_STATUS)) {
-            alert(agl.gl.getShaderInfoLog(fragmentShader));
-            return null;
-        }
+		console.log("atomicGLShader2::createProgram -> compile result: " + agl.gl.getShaderParameter(fragmentShader, agl.gl.COMPILE_STATUS));
+		// check erreur de compilation sans perte de contexte
+		if (!agl.gl.getShaderParameter(fragmentShader, agl.gl.COMPILE_STATUS)
+		&& !agl.gl.isContextLost()) {
+			alert("Error compiling shader:\n" + agl.gl.getShaderInfoLog(fragmentShader));
+			return null;
+		}
 
 		// creation program et link
-        var program = agl.gl.createProgram();
-        agl.gl.attachShader(program, vertexShader);
-        agl.gl.attachShader(program, fragmentShader);
-        agl.gl.linkProgram(program);
+		var program = agl.gl.createProgram();
+		agl.gl.attachShader(program, vertexShader);
+		agl.gl.attachShader(program, fragmentShader);
+		agl.gl.linkProgram(program);
 
 		// debug
 		//console.log("atomicGLShader::createProgram-> link result: "+agl.gl.getProgramParameter(program, agl.gl.LINK_STATUS));
-        if (!agl.gl.getProgramParameter(program, agl.gl.LINK_STATUS)) {
-            alert("atomicGLShader::Could not initialise shaders");
-        }
+		if (!agl.gl.getProgramParameter(program, agl.gl.LINK_STATUS)
+		&& !agl.gl.isContextLost()) {
+			alert("Error linking program:\n" + agl.gl.getProgramInfoLog(program));
+		}
 
 		// attributes
 		//------------------------
@@ -195,31 +198,31 @@ class  atomicGL2MatShader extends atomicGL2Shader{
 		// aVertexColor
 		// aVertexTexCoord
 
-        this.vertexPositionAttribute = agl.gl.getAttribLocation(program, "aVertexPosition");
-				if (this.vertexPositionAttribute == -1) {
-					console.log("atomicGLShader::Could not load attribute aVertexPosition, shader name:"+this.name);
+		this.vertexPositionAttribute = agl.gl.getAttribLocation(program, "aVertexPosition");
+		if (this.vertexPositionAttribute == -1) {
+			console.log("atomicGLShader::Could not load attribute aVertexPosition, shader name:" + this.name);
 
-				} else {
-        agl.gl.enableVertexAttribArray(this.vertexPositionAttribute);
-				}
+		} else {
+			agl.gl.enableVertexAttribArray(this.vertexPositionAttribute);
+		}
 
-        this.vertexNormalAttribute = agl.gl.getAttribLocation(program, "aVertexNormal");
-				if (this.vertexNormalAttribute == -1) {
-					console.log("atomicGLShader::Could not load attribute aVertexNormal, shader name:"+this.name);
+		this.vertexNormalAttribute = agl.gl.getAttribLocation(program, "aVertexNormal");
+		if (this.vertexNormalAttribute == -1) {
+			console.log("atomicGLShader::Could not load attribute aVertexNormal, shader name:" + this.name);
 
-				} else {
-        agl.gl.enableVertexAttribArray(this.vertexNormalAttribute);
-				}
+		} else {
+			agl.gl.enableVertexAttribArray(this.vertexNormalAttribute);
+		}
 
-        this.vertexColorAttribute = agl.gl.getAttribLocation(program, "aVertexColor");
-				if (this.vertexColorAttribute == -1) {
-					console.log("atomicGLShader::Could not load attribute aVertexColor, shader name:"+this.name);
+		this.vertexColorAttribute = agl.gl.getAttribLocation(program, "aVertexColor");
+		if (this.vertexColorAttribute == -1) {
+			console.log("atomicGLShader::Could not load attribute aVertexColor, shader name:" + this.name);
 
-				} else {
-        agl.gl.enableVertexAttribArray(this.vertexColorAttribute);
-				}
+		} else {
+			agl.gl.enableVertexAttribArray(this.vertexColorAttribute);
+		}
 
-		if(this.nbTex>0){
+		if (this.nbTex > 0) {
 			this.texCoordAttribute = agl.gl.getAttribLocation(program, "aVertexTexCoord");
 			agl.gl.enableVertexAttribArray(this.texCoordAttribute);
 		}
@@ -233,9 +236,9 @@ class  atomicGL2MatShader extends atomicGL2Shader{
 		// debug
 		//console.log("atomicGLShader::createProgram - uniforms ");
 		// matrix
-        this.pMatrixUniform = agl.gl.getUniformLocation(program, "uPMatrix");
-        this.mvMatrixUniform = agl.gl.getUniformLocation(program, "uMVMatrix");
-        this.nMatrixUniform = agl.gl.getUniformLocation(program, "uNMatrix");
+		this.pMatrixUniform = agl.gl.getUniformLocation(program, "uPMatrix");
+		this.mvMatrixUniform = agl.gl.getUniformLocation(program, "uMVMatrix");
+		this.nMatrixUniform = agl.gl.getUniformLocation(program, "uNMatrix");
 
 		// lights
 		// uAmbientColor
@@ -244,63 +247,63 @@ class  atomicGL2MatShader extends atomicGL2Shader{
 
 		this.ambientColorUniform = agl.gl.getUniformLocation(program, "uAmbientColor");
 
-        for (var i = 0; i < this.nbLight; i++) {
+		for (var i = 0; i < this.nbLight; i++) {
 			// lights  	position
 			//			color
 			//console.log("atomicGLShader::createProgram - getUniformLocation ->"+"uPointLightPosition"+i);
 			//console.log("atomicGLShader::createProgram - getUniformLocation ->"+"uPointLightColor"+i);
-			this.pointLightLocationUniform[i] = agl.gl.getUniformLocation(program, "uPointLightPosition"+i);
-			this.pointLightColorUniform[i] = agl.gl.getUniformLocation(program, "uPointLightColor"+i);
+			this.pointLightLocationUniform[i] = agl.gl.getUniformLocation(program, "uPointLightPosition" + i);
+			this.pointLightColorUniform[i] = agl.gl.getUniformLocation(program, "uPointLightColor" + i);
 		}
 
 		// textures
 		for (var i = 0; i < this.nbTex; i++) {
 			//console.log("atomicGLShader::createProgram - getUniformLocation ->"+"uSampler"+i);
-			this.samplerUniform[i] = agl.gl.getUniformLocation(program, "uSampler"+i);
+			this.samplerUniform[i] = agl.gl.getUniformLocation(program, "uSampler" + i);
 		}
 
-        return program;
-    }
+		return program;
+	}
 
-    // setUniforms
-    //----------------------------------------
-    // inputs: 	aGL: atomicGLContext
+	// setUniforms
+	//----------------------------------------
+	// inputs: 	aGL: atomicGLContext
 	// 			aMS: atomicGLMatrixStack
-    setUniforms(aGL,aMS){
+	setUniforms(aGL, aMS) {
 		// debug
 		//console.log("atomicGLShader::setUniforms ");
-    	// set this shader as active shader
-    	aGL.gl.useProgram(this.program);
-    	// matrix
-    	//		Projection
-    	// 		Model->view
-    	//		Normal built from Model->view
-    	aGL.gl.uniformMatrix4fv(this.pMatrixUniform, false, aMS.pMatrix);
-        aGL.gl.uniformMatrix4fv(this.mvMatrixUniform, false, aMS.mvMatrix);
+		// set this shader as active shader
+		aGL.gl.useProgram(this.program);
+		// matrix
+		//		Projection
+		// 		Model->view
+		//		Normal built from Model->view
+		aGL.gl.uniformMatrix4fv(this.pMatrixUniform, false, aMS.pMatrix);
+		aGL.gl.uniformMatrix4fv(this.mvMatrixUniform, false, aMS.mvMatrix);
 
-        var normalMatrix = mat3.create();
-        mat4.toInverseMat3(aMS.mvMatrix, normalMatrix);
-        mat3.transpose(normalMatrix);
-        aGL.gl.uniformMatrix3fv(this.nMatrixUniform, false, normalMatrix);
+		var normalMatrix = mat3.create();
+		mat4.toInverseMat3(aMS.mvMatrix, normalMatrix);
+		mat3.transpose(normalMatrix);
+		aGL.gl.uniformMatrix3fv(this.nMatrixUniform, false, normalMatrix);
 
-        // Lights
-        //		ambient
-        aGL.gl.uniform3f(this.ambientColorUniform,aGL.ambientLightColor[0],aGL.ambientLightColor[1],aGL.ambientLightColor[2]);
+		// Lights
+		//		ambient
+		aGL.gl.uniform3f(this.ambientColorUniform, aGL.ambientLightColor[0], aGL.ambientLightColor[1], aGL.ambientLightColor[2]);
 		//		Omni
-		for (var i=0; i < this.nbLight ; i++){
+		for (var i = 0; i < this.nbLight; i++) {
 			// debug
 			//console.log("-- atomicGLShader::setUniforms - Light number ("+i+")");
 			//console.log("-- LightLocation @"+this.pointLightLocationUniform[i]+"::" +aGL.omniLightLocation[i*3+0] +","+ aGL.omniLightLocation[i*3+1]+","+ aGL.omniLightLocation[i*3+2] );
 			//console.log("-- LightColor @"+this.pointLightColorUniform[i]+"::" +aGL.omniLightColor[i*3+0] +","+ aGL.omniLightColor[i*3+1]+","+ aGL.omniLightColor[i*3+2] );
 
-			aGL.gl.uniform3f(this.pointLightLocationUniform[i], aGL.omniLightLocation[i*3+0], aGL.omniLightLocation[i*3+1], aGL.omniLightLocation[i*3+2]);
-			aGL.gl.uniform3f(this.pointLightColorUniform[i],aGL.omniLightColor[i*3+0],aGL.omniLightColor[i*3+1],aGL.omniLightColor[i*3+2]);
+			aGL.gl.uniform3f(this.pointLightLocationUniform[i], aGL.omniLightLocation[i * 3 + 0], aGL.omniLightLocation[i * 3 + 1], aGL.omniLightLocation[i * 3 + 2]);
+			aGL.gl.uniform3f(this.pointLightColorUniform[i], aGL.omniLightColor[i * 3 + 0], aGL.omniLightColor[i * 3 + 1], aGL.omniLightColor[i * 3 + 2]);
 		}
 
 		// textures
-    }
+	}
 
 	// build
 	//-----------------------------
-	build(agl,shaderloader){ this.program = this.createProgram(agl,shaderloader.getVertex(), shaderloader.getFragment()) ;}
+	build(agl, shaderloader) { this.program = this.createProgram(agl, shaderloader.getVertex(), shaderloader.getFragment()); }
 }
--- utils/webgl-utils.js
+++ utils/webgl-utils.js
@@ -57,139 +57,151 @@
  * visible.
  */
 
-WebGLUtils = function() {
+WebGLUtils = function () {
 
-/**
- * Creates the HTLM for a failure message
- * @param {string} canvasContainerId id of container of th
- *        canvas.
- * @return {string} The html.
- */
-var makeFailHTML = function(msg) {
-  return '' +
-    '<table style="background-color: #8CE; width: 100%; height: 100%;"><tr>' +
-    '<td align="center">' +
-    '<div style="display: table-cell; vertical-align: middle;">' +
-    '<div style="">' + msg + '</div>' +
-    '</div>' +
-    '</td></tr></table>';
-};
-
-/**
- * Mesasge for getting a webgl browser
- * @type {string}
- */
-var GET_A_WEBGL_BROWSER = '' +
-  'This page requires a browser that supports WebGL.<br/>' +
-  '<a href="http://get.webgl.org">Click here to upgrade your browser.</a>';
-
-/**
- * Mesasge for need better hardware
- * @type {string}
- */
-var OTHER_PROBLEM = '' +
-  "It doesn't appear your computer can support WebGL.<br/>" +
-  '<a href="http://get.webgl.org/troubleshooting/">Click here for more information.</a>';
-
-/**
- * Creates a webgl context. If creation fails it will
- * change the contents of the container of the <canvas>
- * tag to an error message with the correct links for WebGL.
- * @param {Element} canvas. The canvas element to create a
- *     context from.
- * @param {WebGLContextCreationAttirbutes} opt_attribs Any
- *     creation attributes you want to pass in.
- * @param {function:(msg)} opt_onError An function to call
- *     if there is an error during creation.
- * @return {WebGLRenderingContext} The created context.
- */
-var setupWebGL = function(canvas, opt_attribs, opt_onError) {
-  function handleCreationError(msg) {
-    var container = canvas.parentNode;
-    if (container) {
-      var str = window.WebGLRenderingContext ?
-           OTHER_PROBLEM :
-           GET_A_WEBGL_BROWSER;
-      if (msg) {
-        str += "<br/><br/>Status: " + msg;
-      }
-      container.innerHTML = makeFailHTML(str);
-    }
+  /**
+   * Creates the HTLM for a failure message
+   * @param {string} canvasContainerId id of container of th
+   *        canvas.
+   * @return {string} The html.
+   */
+  var makeFailHTML = function (msg) {
+    return '' +
+      '<table style="background-color: #8CE; width: 100%; height: 100%;"><tr>' +
+      '<td align="center">' +
+      '<div style="display: table-cell; vertical-align: middle;">' +
+      '<div style="">' + msg + '</div>' +
+      '</div>' +
+      '</td></tr></table>';
   };
 
-  opt_onError = opt_onError || handleCreationError;
+  /**
+   * Mesasge for getting a webgl browser
+   * @type {string}
+   */
+  var GET_A_WEBGL_BROWSER = '' +
+    'This page requires a browser that supports WebGL.<br/>' +
+    '<a href="http://get.webgl.org">Click here to upgrade your browser.</a>';
 
-  if (canvas.addEventListener) {
-    canvas.addEventListener("webglcontextcreationerror", function(event) {
-          opt_onError(event.statusMessage);
-        }, false);
-  }
-  var context = create3DContext(canvas, opt_attribs);
-  if (!context) {
-    if (!window.WebGLRenderingContext) {
-      opt_onError("");
+  /**
+   * Mesasge for need better hardware
+   * @type {string}
+   */
+  var OTHER_PROBLEM = '' +
+    "It doesn't appear your computer can support WebGL.<br/>" +
+    '<a href="http://get.webgl.org/troubleshooting/">Click here for more information.</a>';
+
+  /**
+   * Creates a webgl context. If creation fails it will
+   * change the contents of the container of the <canvas>
+   * tag to an error message with the correct links for WebGL.
+   * @param {Element} canvas. The canvas element to create a
+   *     context from.
+   * @param {WebGLContextCreationAttirbutes} opt_attribs Any
+   *     creation attributes you want to pass in.
+   * @param {function:(msg)} opt_onError An function to call
+   *     if there is an error during creation.
+   * @return {WebGLRenderingContext} The created context.
+   */
+  var setupWebGL = function (canvas, opt_attribs, opt_onError) {
+    function handleCreationError(msg) {
+      var container = canvas.parentNode;
+      if (container) {
+        var str = window.WebGLRenderingContext ?
+          OTHER_PROBLEM :
+          GET_A_WEBGL_BROWSER;
+        if (msg) {
+          str += "<br/><br/>Status: " + msg;
+        }
+        container.innerHTML = makeFailHTML(str);
+      }
+    };
+
+    opt_onError = opt_onError || handleCreationError;
+
+    if (canvas.addEventListener) {
+      canvas.addEventListener("webglcontextcreationerror", function (event) {
+        opt_onError(event.statusMessage);
+      }, false);
     }
-  }
-  return context;
-};
-
-/**
- * Creates a webgl context.
- * @param {!Canvas} canvas The canvas tag to get context
- *     from. If one is not passed in one will be created.
- * @return {!WebGLContext} The created context.
- */
-var create3DContext = function(canvas, opt_attribs) {
-  var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
-  var context = null;
-  for (var ii = 0; ii < names.length; ++ii) {
-    try {
-      context = canvas.getContext(names[ii], opt_attribs);
-    } catch(e) {}
-    if (context) {
-      break;
+    var context = create3DContext(canvas, opt_attribs);
+    if (!context) {
+      if (!window.WebGLRenderingContext) {
+        opt_onError("");
+      }
     }
-  }
-  return context;
-}
+    return context;
+  };
 
-return {
-  create3DContext: create3DContext,
-  setupWebGL: setupWebGL
-};
+  /**
+   * Creates a webgl context.
+   * @param {!Canvas} canvas The canvas tag to get context
+   *     from. If one is not passed in one will be created.
+   * @return {!WebGLContext} The created context.
+   */
+  var create3DContext = function (canvas, opt_attribs) {
+    var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
+    var context = null;
+    for (var ii = 0; ii < names.length; ++ii) {
+      try {
+        context = canvas.getContext(names[ii], opt_attribs);
+      } catch (e) { }
+      if (context) {
+        break;
+      }
+    }
+    return context;
+  }
+
+  return {
+    create3DContext: create3DContext,
+    setupWebGL: setupWebGL
+  };
 }();
 
 /**
  * Provides requestAnimationFrame in a cross browser way.
  */
-window.requestAnimFrame = (function() {
+window.requestAnimFrame = (function () {
   return window.requestAnimationFrame ||
-         window.webkitRequestAnimationFrame ||
-         window.mozRequestAnimationFrame ||
-         window.oRequestAnimationFrame ||
-         window.msRequestAnimationFrame ||
-         function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
-           window.setTimeout(callback, 1000/60);
-         };
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+      window.setTimeout(callback, 1000 / 60);
+    };
 })();
 
-  /**
-   * Resize a canvas to match the size its displayed.
-   * @param {HTMLCanvasElement} canvas The canvas to resize.
-   * @param {number} [multiplier] amount to multiply by.
-   *    Pass in window.devicePixelRatio for native pixels.
-   * @return {boolean} true if the canvas was resized.
-   * @memberOf module:webgl-utils
-   */
-  function resizeCanvasToDisplaySize(canvas, multiplier) {
-    multiplier = multiplier || 1;
-    var width  = canvas.clientWidth  * multiplier | 0;
-    var height = canvas.clientHeight * multiplier | 0;
-    if (canvas.width !== width ||  canvas.height !== height) {
-      canvas.width  = width;
-      canvas.height = height;
-      return true;
-    }
-    return false;
+/**
+ * Provides cancelAnimationFrame in a cross browser way.
+ */
+window.cancelRequestAnimFrame = (function () {
+  return window.cancelAnimationFrame ||
+    window.webkitCancelRequestAnimationFrame ||
+    window.mozCancelRequestAnimationFrame ||
+    window.oCancelRequestAnimationFrame ||
+    window.msCancelRequestAnimationFrame ||
+    clearTimeout
+})();
+
+/**
+ * Resize a canvas to match the size its displayed.
+ * @param {HTMLCanvasElement} canvas The canvas to resize.
+ * @param {number} [multiplier] amount to multiply by.
+ *    Pass in window.devicePixelRatio for native pixels.
+ * @return {boolean} true if the canvas was resized.
+ * @memberOf module:webgl-utils
+ */
+function resizeCanvasToDisplaySize(canvas, multiplier) {
+  multiplier = multiplier || 1;
+  var width = canvas.clientWidth * multiplier | 0;
+  var height = canvas.clientHeight * multiplier | 0;
+  if (canvas.width !== width || canvas.height !== height) {
+    canvas.width = width;
+    canvas.height = height;
+    return true;
   }
+  return false;
+}
 
