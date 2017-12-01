af38354f4c1775084d2cbecc4634c7433b569ce9
nealith <nealith@neaworld.fr>
2017-12-01 08:51:17 +0100

    correct mistake, optimize code

--- objtojs.py
+++ objtojs.py
@@ -16,9 +16,9 @@ def convert(path):
     v = []
     vn = []
     vt = []
+    f = []
 
-    indexcount = 0
-    indexdic = {}
+    # 0(n)  (n mots)
 
     # Get all data for treatement
     for line in arrayLines :
@@ -27,23 +27,61 @@ def convert(path):
             name = arrayLine[1]
         if arrayLine[0] == 'f':
 
-            for value in arrayLine[1:len(arrayLine)]:
+            tri = []
 
-                fData = value.split('/')
+            if len(arrayLine) == 4:
+                for value in arrayLine[1:len(arrayLine)]:
 
-                key = ()
+                    fData = value.split('/')
 
-                if fData[1] == '':
-                    key = (int(fData[0]),-1,int(fData[2]))
-                else:
-                    key = (int(fData[0]),int(fData[1]),int(fData[2]))
+                    ver = ()
 
-                if key in indexdic.keys():
-                    indexdic[key].append(indexcount)
-                else:
-                    indexdic[key] = [indexcount]
+                    if fData[1] == '':
+                        ver = (int(fData[0]),-1,int(fData[2]))
+                    else:
+                        ver = (int(fData[0]),int(fData[1]),int(fData[2]))
 
-            indexcount += 1
+                    tri.append(ver)
+
+                f.append(tri)
+
+            if len(arrayLine) == 5:
+                for value in arrayLine[1:len(arrayLine)-1]:
+
+                    fData = value.split('/')
+
+                    ver = ()
+
+                    if fData[1] == '':
+                        ver = (int(fData[0]),-1,int(fData[2]))
+                    else:
+                        ver = (int(fData[0]),int(fData[1]),int(fData[2]))
+
+                    tri.append(ver)
+
+                f.append(tri)
+                tri = []
+
+                tmp = [arrayLine[1],arrayLine[3],arrayLine[4]]
+
+                for value in tmp:
+
+                    fData = value.split('/')
+
+                    ver = ()
+
+                    if fData[1] == '':
+                        ver = (int(fData[0]),-1,int(fData[2]))
+                    else:
+                        ver = (int(fData[0]),int(fData[1]),int(fData[2]))
+
+                    tri.append(ver)
+
+                f.append(tri)
+
+
+
+
 
         if arrayLine[0] == 'vn':
             vn.append((float(arrayLine[1]),float(arrayLine[2]),float(arrayLine[3])))
@@ -62,15 +100,9 @@ def convert(path):
     valuesindex = {}
     valuesindexcount = 0
 
-    for i in range(0,indexcount):
-        t = []
+    for t in f:
+
         tindex = []
-        # Find the tree group of value that define a triangle
-
-        for key in indexdic.keys():
-            if i in indexdic[key]:
-                t.append(key)
-
         # Give a index to each group, add their values in the ordre to each array
         for ts in t:
             if ts in valuesindex.keys():
@@ -99,67 +131,70 @@ def convert(path):
 
                 valuesindexcount += 1
 
-
-        # Add their index to index
-
         for j in tindex:
             index.append(j)
 
-        newpath = path[0:len(path)-3] + "js"
-
-        js = open(newpath,"w")
-
-        js.write(name+" = function(){"+"\r\n")
 
 
-        #vertices
-        js.write("this.vertices = ["+"\r\n")
-        verticecount = 0
-        for vertice in vertices:
-            if verticecount < len(vertices)-1:
-                js.write(str(vertice[0])+","+str(vertice[1])+","+str(vertice[2])+", // vertice"+str(verticecount)+"\r\n")
-            else:
-                js.write(str(vertice[0])+","+str(vertice[1])+","+str(vertice[2])+" // vertice"+str(verticecount)+"\r\n")
-            verticecount += 1
-
-        js.write("];"+"\r\n")
-
-        #normals
-        js.write("this.normals = ["+"\r\n")
-        normalcount = 0
-        for normal in normals:
-            if normalcount < len(normals)-1:
-                js.write(str(normal[0])+","+str(normal[1])+","+str(normal[2])+", // normal"+str(normalcount)+"\r\n")
-            else:
-                js.write(str(normal[0])+","+str(normal[1])+","+str(normal[2])+" // normal"+str(normalcount)+"\r\n")
-            normalcount += 1
-
-        js.write("];"+"\r\n")
-
-        #uv
-        js.write("this.uv = ["+"\r\n")
-        uvcount = 0
-        for uv_ele in uv:
-            if uvcount < len(uv)-1:
-                js.write(str(uv_ele[0])+","+str(uv_ele[1])+", // uv"+str(uvcount)+"\r\n")
-            else:
-                js.write(str(uv_ele[0])+","+str(uv_ele[1])+" // uv"+str(uvcount)+"\r\n")
-            uvcount += 1
-
-        js.write("];"+"\r\n")
-
-        #index
-        js.write("this.index = ["+"\r\n")
-        indexcount = 0
-        for i in index:
-            if indexcount < len(index)-1:
-                js.write(str(i)+","+"\r\n")
-            else:
-                js.write(str(i)+"\r\n")
-        js.write("];"+"\r\n")
 
 
-        js.write("}")
 
 
-        js.close()
+    newpath = path[0:len(path)-3] + "js"
+
+    js = open(newpath,"w")
+
+    js.write(name+" = function(){"+"\r\n")
+
+
+    #vertices
+    js.write("this.vertices = ["+"\r\n")
+    verticecount = 0
+    for vertice in vertices:
+        if verticecount < len(vertices)-1:
+            js.write(str(vertice[0])+","+str(vertice[1])+","+str(vertice[2])+", // vertice"+str(verticecount)+"\r\n")
+        else:
+            js.write(str(vertice[0])+","+str(vertice[1])+","+str(vertice[2])+" // vertice"+str(verticecount)+"\r\n")
+        verticecount += 1
+
+    js.write("];"+"\r\n")
+
+    #normals
+    js.write("this.normals = ["+"\r\n")
+    normalcount = 0
+    for normal in normals:
+        if normalcount < len(normals)-1:
+            js.write(str(normal[0])+","+str(normal[1])+","+str(normal[2])+", // normal"+str(normalcount)+"\r\n")
+        else:
+            js.write(str(normal[0])+","+str(normal[1])+","+str(normal[2])+" // normal"+str(normalcount)+"\r\n")
+        normalcount += 1
+
+    js.write("];"+"\r\n")
+
+    #uv
+    js.write("this.uv = ["+"\r\n")
+    uvcount = 0
+    for uv_ele in uv:
+        if uvcount < len(uv)-1:
+            js.write(str(uv_ele[0])+","+str(uv_ele[1])+", // uv"+str(uvcount)+"\r\n")
+        else:
+            js.write(str(uv_ele[0])+","+str(uv_ele[1])+" // uv"+str(uvcount)+"\r\n")
+        uvcount += 1
+
+    js.write("];"+"\r\n")
+
+    #index
+    js.write("this.index = ["+"\r\n")
+    indexcount = 0
+    for i in index:
+        if indexcount < len(index)-1:
+            js.write(str(i)+","+"\r\n")
+        else:
+            js.write(str(i)+"\r\n")
+    js.write("];"+"\r\n")
+
+
+    js.write("}")
+
+
+    js.close()
