e5dcbdf79f9e4798f134a2df6cc96aac62785de7
nealith <nealith@neaworld.fr>
2017-12-17 15:11:06 +0100

    correction1

--- objtojs.py
+++ objtojs.py
@@ -58,38 +58,39 @@ def convertFile(path):
                 f.append(tri)
 
             if len(arrayLine) == 5:
-                for value in arrayLine[1:len(arrayLine)-1]:
+                if arrayLine[4] != '':
+                    for value in arrayLine[1:len(arrayLine)-1]:
 
-                    fData = value.split('/')
+                        fData = value.split('/')
 
-                    ver = ()
+                        ver = ()
 
-                    if fData[1] == '':
-                        ver = (int(fData[0]),-1,int(fData[2]))
-                    else:
-                        ver = (int(fData[0]),int(fData[1]),int(fData[2]))
+                        if fData[1] == '':
+                            ver = (int(fData[0]),-1,int(fData[2]))
+                        else:
+                            ver = (int(fData[0]),int(fData[1]),int(fData[2]))
 
-                    tri.append(ver)
+                        tri.append(ver)
 
-                f.append(tri)
-                tri = []
+                    f.append(tri)
+                    tri = []
 
-                tmp = [arrayLine[1],arrayLine[3],arrayLine[4]]
+                    tmp = [arrayLine[1],arrayLine[3],arrayLine[4]]
 
-                for value in tmp:
+                    for value in tmp:
 
-                    fData = value.split('/')
+                        fData = value.split('/')
 
-                    ver = ()
+                        ver = ()
 
-                    if fData[1] == '':
-                        ver = (int(fData[0]),-1,int(fData[2]))
-                    else:
-                        ver = (int(fData[0]),int(fData[1]),int(fData[2]))
+                        if fData[1] == '':
+                            ver = (int(fData[0]),-1,int(fData[2]))
+                        else:
+                            ver = (int(fData[0]),int(fData[1]),int(fData[2]))
 
-                    tri.append(ver)
+                        tri.append(ver)
 
-                f.append(tri)
+                    f.append(tri)
 
 
 
